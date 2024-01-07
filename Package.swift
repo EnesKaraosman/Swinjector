85b9032ab164dc86b6e18ded4f24f2a1ad85ce4d
Enes Karaosman <eneskaraosman53@gmail.com>
2024-01-07 12:13:57 +0300

    feat: specify platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Swinjector",
+    platforms: [.iOS(.v15), .macOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
