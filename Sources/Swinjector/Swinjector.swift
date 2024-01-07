a47f55f70ee19cdbd459c07227368baf123b8f50
Enes Karaosman <eneskaraosman53@gmail.com>
2024-01-07 09:32:23 +0300

    initial implementation of Swinjector

--- Sources/Swinjector/Swinjector.swift
+++ Sources/Swinjector/Swinjector.swift
@@ -1,2 +1,81 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+public protocol DependencyContainer {
+    func register<T>(key: StaticString, _ factory: @escaping () -> T)
+    func registerFactory<T>(_ serviceType: T.Type, factory: @escaping () -> T)
+    func registerSingleton<T>(_ serviceType: T.Type, instance: T)
+    func registerLazySingleton<T>(_ serviceType: T.Type, factory: @escaping () -> T)
+
+    func resolve<T>(_ serviceType: T.Type) -> T?
+    func unregister<T>(_ serviceType: T.Type)
+    func isRegistered<T>(_ serviceType: T.Type) -> Bool
+    func reset()
+}
+
+
+public class GetIt: DependencyContainer {
+    public static let I = GetIt()
+
+    private init() {}
+
+    private var factories: [String: () -> Any] = [:]
+    private var singletons: [String: Any] = [:]
+    private var lazySingletons: [String: () -> Any] = [:]
+
+    public func register<T>(key: StaticString = #function, _ factory: @escaping () -> T) {
+        let key = String(reflecting: key)
+        factories[key] = factory
+    }
+    
+    public func registerFactory<T>(_ serviceType: T.Type, factory: @escaping () -> T) {
+        let key = String(describing: serviceType)
+        factories[key] = factory
+    }
+
+    public func registerSingleton<T>(_ serviceType: T.Type, instance: T) {
+        let key = String(describing: serviceType)
+        singletons[key] = instance
+    }
+
+    public func registerLazySingleton<T>(_ serviceType: T.Type, factory: @escaping () -> T) {
+        let key = String(describing: serviceType)
+        lazySingletons[key] = factory
+    }
+
+    public func resolve<T>(_ serviceType: T.Type) -> T? {
+        let key = String(describing: serviceType)
+
+        if let singleton = singletons[key] as? T {
+            return singleton
+        } else if let factory = factories[key] {
+            let instance = factory() as! T
+            return instance
+        } else if let lazyFactory = lazySingletons[key] {
+            let instance = lazyFactory() as! T
+            singletons[key] = instance
+            return instance
+        }
+
+        return nil
+    }
+
+    public func unregister<T>(_ serviceType: T.Type) {
+        let key = String(describing: serviceType)
+        factories.removeValue(forKey: key)
+        singletons.removeValue(forKey: key)
+        lazySingletons.removeValue(forKey: key)
+    }
+
+    public func reset() {
+        factories.removeAll()
+        singletons.removeAll()
+        lazySingletons.removeAll()
+    }
+    
+    public func isRegistered<T>(_ serviceType: T.Type) -> Bool {
+           let key = String(describing: serviceType)
+           return factories[key] != nil || singletons[key] != nil || lazySingletons[key] != nil
+       }
+    
+    func callAsFunction<T>(_ serviceType: T.Type) -> T? {
+        resolve(serviceType)
+    }
+}
--- Tests/SwinjectorTests/SwinjectorTests.swift
+++ Tests/SwinjectorTests/SwinjectorTests.swift
@@ -1,12 +1,67 @@
 import XCTest
 @testable import Swinjector
 
-final class SwinjectorTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+protocol TestProtocol {
+    func test() -> Void
+}
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+class TestClass: TestProtocol, Hashable {
+    static func == (lhs: TestClass, rhs: TestClass) -> Bool {
+        ObjectIdentifier(lhs) == ObjectIdentifier(rhs)
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(ObjectIdentifier(self))
+    }
+    
+    func test() {
+        debugPrint("Tested")
+    }
+}
+
+final class SwinjectorTests: XCTestCase {
+    func testRegisterFactoryIsRegistered() throws {
+        GetIt.I.reset()
+        
+        GetIt.I.registerFactory(TestProtocol.self) {
+            TestClass()
+        }
+        
+        XCTAssertTrue(GetIt.I.isRegistered(TestProtocol.self))
+    }
+    
+    func testRegisterSingletonIsRegistered() throws {
+        GetIt.I.reset()
+        
+        GetIt.I.registerSingleton(TestProtocol.self, instance: TestClass())
+        
+        XCTAssertTrue(GetIt.I.isRegistered(TestProtocol.self))
+    }
+    
+    func testRegisterLazySingletonIsRegistered() throws {
+        GetIt.I.reset()
+        
+        GetIt.I.registerLazySingleton(TestProtocol.self) {
+            TestClass()
+        }
+        
+        XCTAssertTrue(GetIt.I.isRegistered(TestProtocol.self))
+    }
+    
+    func testRegisterLazySingletonIsReallyASingleton() throws {
+        GetIt.I.reset()
+        
+        let clazz = TestClass()
+        
+        GetIt.I.registerLazySingleton(TestProtocol.self) {
+            clazz
+        }
+        
+        print(clazz.hashValue)
+        
+        guard let getClazz = GetIt.I(TestProtocol.self) as? TestClass else { return }
+        print(getClazz.hashValue)
+        
+        XCTAssertEqual(clazz, getClazz)
     }
 }
